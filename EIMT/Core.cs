b38b6dab91a03afb63c83b81c40a6ee44fdabe22
chimingkuei <chimingkuei@gmail.com>
2023-11-13 00:26:50 +0800

    EIMT

---
+++ EIMT/Core.cs
@@ -0,0 +1,190 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EIMT
+{
+    interface IData
+    {
+        IEnumerable<(string, string[], string[])> Walk(string root_path);
+        void CheckDir(string path);
+        bool CheckFileFormat(string path);
+    }
+
+    class Core: IData
+    {
+        public IEnumerable<(string, string[], string[])> Walk(string root_path)
+        {
+            // 遍歷當前目錄下的所有文件和子目錄
+            string[] files = Directory.GetFiles(root_path);
+            string[] subDirs = Directory.GetDirectories(root_path);
+            yield return (root_path, subDirs, files);
+
+            // 遞歸遍歷子目錄
+            foreach (string subDir in subDirs)
+            {
+                foreach (var tuple in Walk(subDir))
+                {
+                    yield return tuple;
+                }
+            }
+        }
+
+        public void CheckDir(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public void ConvertOR(Mat src, string output_file, double original_OR, double target_OR)
+        {
+            double magnification = original_OR / target_OR;
+            Mat result_image = new Mat();
+            Cv2.Resize(src, result_image, new OpenCvSharp.Size(Convert.ToInt32(src.Width * magnification), Convert.ToInt32(src.Height * magnification)));
+            Cv2.ImWrite(output_file, result_image);
+        }
+
+        private Mat ResizeImage(Mat src, double magnification)
+        {
+            Mat result_image = new Mat();
+            Cv2.Resize(src, result_image, new OpenCvSharp.Size(Convert.ToInt32(src.Width * magnification), Convert.ToInt32(src.Height * magnification)));
+            return result_image;
+        }
+
+        public bool CheckFileFormat(string path)
+        {
+            bool state = true;
+            string[] fileNames = Directory.GetFiles(path);
+            foreach (string fileName in fileNames)
+            {
+                string fileExtension = Path.GetExtension(fileName).ToLower();
+                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+                if (fileExtension != ".bmp" || !Int32.TryParse(fileNameWithoutExtension, out int result))
+                {
+                    state = false;
+                }
+            }
+            return state;
+        }
+
+        public void Stitching(string input_dir, string output_dir, string direction, OpenCvSharp.ImreadModes color_type, double magnification)
+        {
+            int img_num = Directory.GetFiles(input_dir).Length;
+            Mat result_image = new Mat();
+            switch (direction)
+            {
+                case "Bottom to Top":
+                    {
+                        for (int i = 2; i <= img_num; i++)
+                        {
+                            if (i == 2)
+                            {
+                                Mat img1 = new Mat(Path.Combine(input_dir, "1.bmp"), color_type);
+                                Mat img2 = new Mat(Path.Combine(input_dir, "2.bmp"), color_type);
+                                Mat img1_resize = ResizeImage(img1, magnification);
+                                Mat img2_resize = ResizeImage(img2, magnification);
+                                Cv2.VConcat(new Mat[] { img2_resize, img1_resize }, result_image);
+                            }
+                            else
+                            {
+                                Mat img = new Mat(Path.Combine(input_dir, i.ToString() + ".bmp"), color_type);
+                                Mat img_resize = ResizeImage(img, magnification);
+                                Cv2.VConcat(new Mat[] { img_resize, result_image }, result_image);
+                            }
+                        }
+                        Cv2.ImWrite(Path.Combine(output_dir, "VConcat.bmp"), result_image);
+                        break;
+                    }
+                case "Top to Bottom":
+                    {
+                        for (int i = 2; i <= img_num; i++)
+                        {
+                            if (i == 2)
+                            {
+                                Mat img1 = new Mat(Path.Combine(input_dir, "1.bmp"), color_type);
+                                Mat img2 = new Mat(Path.Combine(input_dir, "2.bmp"), color_type);
+                                Mat img1_resize = ResizeImage(img1, magnification);
+                                Mat img2_resize = ResizeImage(img2, magnification);
+                                Cv2.VConcat(new Mat[] { img1_resize, img2_resize }, result_image);
+                            }
+                            else
+                            {
+                                Mat img = new Mat(Path.Combine(input_dir, i.ToString() + ".bmp"), color_type);
+                                Mat img_resize = ResizeImage(img, magnification);
+                                Cv2.VConcat(new Mat[] { result_image, img_resize }, result_image);
+                            }
+                        }
+                        Cv2.ImWrite(Path.Combine(output_dir, "VConcat.bmp"), result_image);
+                        break;
+                    }
+                case "Left to Right":
+                    {
+                        for (int i = 2; i <= img_num; i++)
+                        {
+                            if (i == 2)
+                            {
+                                Mat img1 = new Mat(Path.Combine(input_dir, "1.bmp"), color_type);
+                                Mat img2 = new Mat(Path.Combine(input_dir, "2.bmp"), color_type);
+                                Mat img1_resize = ResizeImage(img1, magnification);
+                                Mat img2_resize = ResizeImage(img2, magnification);
+                                Cv2.HConcat(new Mat[] { img1_resize, img2_resize }, result_image);
+                            }
+                            else
+                            {
+                                Mat img = new Mat(Path.Combine(input_dir, i.ToString() + ".bmp"), color_type);
+                                Mat img_resize = ResizeImage(img, magnification);
+                                Cv2.HConcat(new Mat[] { result_image, img_resize }, result_image);
+                            }
+                        }
+                        Cv2.ImWrite(Path.Combine(output_dir, "HConcat.bmp"), result_image);
+                        break;
+                    }
+                case "Right to Left":
+                    {
+                        for (int i = 2; i <= img_num; i++)
+                        {
+                            if (i == 2)
+                            {
+                                Mat img1 = new Mat(Path.Combine(input_dir, "1.bmp"), color_type);
+                                Mat img2 = new Mat(Path.Combine(input_dir, "2.bmp"), color_type);
+                                Mat img1_resize = ResizeImage(img1, magnification);
+                                Mat img2_resize = ResizeImage(img2, magnification);
+                                Cv2.HConcat(new Mat[] { img2_resize, img1_resize }, result_image);
+                            }
+                            else
+                            {
+                                Mat img = new Mat(Path.Combine(input_dir, i.ToString() + ".bmp"), color_type);
+                                Mat img_resize = ResizeImage(img, magnification);
+                                Cv2.HConcat(new Mat[] { img_resize, result_image }, result_image);
+                            }
+                        }
+                        Cv2.ImWrite(Path.Combine(output_dir, "HConcat.bmp"), result_image);
+                        break;
+                    }
+            }
+        }
+
+        public void MoveCutImage(Mat src, string output_dir, string file_name, Size size)
+        {
+            int col = (int)Math.Floor((double)(src.Width / size.Width));
+            int row = (int)Math.Floor((double)(src.Height / size.Height));
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    Rect roi = new Rect(i * size.Width, j * size.Height, size.Width, size.Height);
+                    Mat croppedImage = new Mat(src, roi);
+                    Cv2.ImWrite(Path.Combine(output_dir, file_name + "_" + i.ToString() + "," + j.ToString() + ".bmp"), croppedImage);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ EIMT/MainWindow.xaml.cs
@@ -0,0 +1,356 @@
+﻿using Newtonsoft.Json.Linq;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EIMT
+{
+    public class Parameter
+    {
+        #region Main Screen Parameter
+        public string Source_Folder_val { get; set; }
+        public string Output_Folder_val { get; set; }
+        #endregion
+        #region Conversion OR Parameter
+        public string Source_OR_val { get; set; }
+        public string Output_OR_val { get; set; }
+
+        public bool OR_Color_val { get; set; }
+        public bool OR_Gray_val { get; set; }
+
+        public string Thread_Num_val { get; set; }
+        #endregion
+        #region Stitching Parameter
+        public bool Stitching_Top_to_Bottom_val { get; set; }
+        public bool Stitching_Bottom_to_Top_val { get; set; }
+        public bool Stitching_Left_to_Right_val { get; set; }
+        public bool Stitching_Right_to_Left_val { get; set; }
+        public bool Stitching_Color_val { get; set; }
+        public bool Stitching_Gray_val { get; set; }
+        public string Magnification_val { get; set; }
+        #endregion
+        #region Moving Cutting Image Parameter
+        public string Crop_Length_val { get; set; }
+        public string Crop_Width_val { get; set; }
+        #endregion
+    }
+
+    public partial class MainWindow : System.Windows.Window
+    {
+        // Evaluate Image Modification Tool
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void OpenDir(TextBox Control)
+        {
+            System.Windows.Forms.FolderBrowserDialog path = new System.Windows.Forms.FolderBrowserDialog();
+            path.ShowDialog();
+            Control.Text = path.SelectedPath;
+        }
+
+        private string AddRootPath(string originalpath, string targetpath)
+        {
+            string[] pathParts = Regex.Split(originalpath, @"\\|/");
+            pathParts[0] = targetpath;
+            string new_originalpath = string.Join("\\", pathParts);
+            //Console.WriteLine(new_originalpath);
+            return new_originalpath;
+        }
+
+        #region Config
+        private void LoadConfig()
+        {
+            List<Parameter> Parameter_info = Config.Load();
+            #region Main Screen Parameter
+            Source_Folder.Text = Parameter_info[0].Source_Folder_val;
+            Output_Folder.Text = Parameter_info[0].Output_Folder_val;
+            #endregion
+            #region Conversion OR Parameter
+            Source_OR.Text = Parameter_info[0].Source_OR_val;
+            Output_OR.Text = Parameter_info[0].Output_OR_val;
+            OR_Color.IsChecked = Parameter_info[0].OR_Color_val;
+            OR_Gray.IsChecked = Parameter_info[0].OR_Gray_val;
+            Thread_Num.Text = Parameter_info[0].Thread_Num_val;
+            #endregion
+            #region Stitching Parameter
+            Stitching_Top_to_Bottom.IsChecked = Parameter_info[0].Stitching_Top_to_Bottom_val;
+            Stitching_Bottom_to_Top.IsChecked = Parameter_info[0].Stitching_Bottom_to_Top_val;
+            Stitching_Left_to_Right.IsChecked = Parameter_info[0].Stitching_Left_to_Right_val;
+            Stitching_Right_to_Left.IsChecked = Parameter_info[0].Stitching_Right_to_Left_val;
+            Stitching_Color.IsChecked = Parameter_info[0].Stitching_Color_val;
+            Stitching_Gray.IsChecked = Parameter_info[0].Stitching_Gray_val;
+            Magnification.Text = Parameter_info[0].Magnification_val;
+            #endregion
+            #region Moving Cutting Image Parameter
+            Crop_Length.Text = Parameter_info[0].Crop_Length_val;
+            Crop_Width.Text = Parameter_info[0].Crop_Width_val;
+            #endregion
+        }
+
+        private void SaveConfig()
+        {
+            List<Parameter> Parameter_config = new List<Parameter>()
+                        {
+                            new Parameter()
+                            {
+                                #region Main Screen Parameter
+                                Source_Folder_val = Source_Folder.Text,
+                                Output_Folder_val = Output_Folder.Text,
+                                #endregion
+                                #region Conversion OR Parameter
+                                Source_OR_val = Source_OR.Text,
+                                Output_OR_val = Output_OR.Text,
+                                OR_Color_val=(bool)OR_Color.IsChecked,
+                                OR_Gray_val=(bool)OR_Gray.IsChecked,
+                                Thread_Num_val= Thread_Num.Text,
+                                #endregion
+                                #region Stitching Parameter
+                                Stitching_Top_to_Bottom_val = (bool)Stitching_Top_to_Bottom.IsChecked,
+                                Stitching_Bottom_to_Top_val = (bool)Stitching_Bottom_to_Top.IsChecked,
+                                Stitching_Left_to_Right_val = (bool)Stitching_Left_to_Right.IsChecked,
+                                Stitching_Right_to_Left_val = (bool)Stitching_Right_to_Left.IsChecked,
+                                Stitching_Color_val = (bool)Stitching_Color.IsChecked,
+                                Stitching_Gray_val = (bool)Stitching_Gray.IsChecked,
+                                Magnification_val = Magnification.Text,
+                                #endregion
+                                #region Moving Cutting Image Parameter
+                                Crop_Length_val = Crop_Length.Text,
+                                Crop_Width_val = Crop_Width.Text,
+                                #endregion
+        }
+    };
+            Config.Save(Parameter_config);
+            Logger.WriteLog("儲存參數!", 1, richTextBoxGeneral);
+        }
+        #endregion
+
+        #region Dispatcher GetValue or SetValue
+        public string TextBoxDispatcherGetValue(TextBox control)
+        {
+            string name = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                name = control.Text;
+            });
+            return name;
+
+        }
+
+        public double DoubleUpDownDispatcherGetValue(Xceed.Wpf.Toolkit.DoubleUpDown control)
+        {
+            double value = new double();
+            this.Dispatcher.Invoke(() =>
+            {
+                value = Convert.ToDouble(control.Text);
+            });
+            return value;
+        }
+
+        public int IntegerUpDownDispatcherGetValue(Xceed.Wpf.Toolkit.IntegerUpDown control)
+        {
+            int value = new int();
+            this.Dispatcher.Invoke(() =>
+            {
+                value = Convert.ToInt32(control.Text);
+            });
+            return value;
+        }
+
+        public bool RadioButtonDispatcherGetValue(RadioButton control)
+        {
+            bool value = new bool();
+            this.Dispatcher.Invoke(() =>
+            {
+                value = (bool)control.IsChecked;
+            });
+            return value;
+
+        }
+        #endregion
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig();
+        }
+        BaseLogRecord Logger = new BaseLogRecord();
+        BaseConfig<Parameter> Config = new BaseConfig<Parameter>();
+        Core Do = new Core();
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Open_Source_Folder):
+                    {
+                        OpenDir(Source_Folder);
+                        Logger.WriteLog("開啟影像資料夾路徑:" + Source_Folder.Text + "!", 1, richTextBoxGeneral);
+                        break;
+                    }
+                case nameof(Open_Output_Folder):
+                    {
+                        OpenDir(Output_Folder);
+                        Logger.WriteLog("開啟目的資料夾路徑:" + Output_Folder.Text + "!", 1, richTextBoxGeneral);
+                        break;
+                    }
+                case nameof(OR_Scaling):
+                    {
+                        if (!string.IsNullOrEmpty(Source_Folder.Text))
+                        {
+                            if (!string.IsNullOrEmpty(Output_Folder.Text))
+                            {
+                                Task.Run(() =>
+                                {
+                                    var type = RadioButtonDispatcherGetValue(OR_Color) ? OpenCvSharp.ImreadModes.Color : OpenCvSharp.ImreadModes.Grayscale;
+                                    foreach (var (dirPath, subDirs, files) in Do.Walk(TextBoxDispatcherGetValue(Source_Folder)))
+                                    {
+                                        Do.CheckDir(AddRootPath(dirPath, TextBoxDispatcherGetValue(Output_Folder)));
+                                        ParallelOptions options = new ParallelOptions();
+                                        options.MaxDegreeOfParallelism = IntegerUpDownDispatcherGetValue(Thread_Num);
+                                        Parallel.ForEach(files, options, filePath =>
+                                        {
+                                            OpenCvSharp.Mat src = new OpenCvSharp.Mat(filePath, type);
+                                            if (!src.Empty())
+                                            {
+                                                string newfilePath = AddRootPath(filePath, TextBoxDispatcherGetValue(Output_Folder));
+                                                Do.ConvertOR(src, newfilePath, DoubleUpDownDispatcherGetValue(Source_OR), DoubleUpDownDispatcherGetValue(Output_OR));
+                                            }
+                                        }
+                                        );
+                                    }
+                                    this.Dispatcher.Invoke(() =>
+                                    {
+                                        Logger.WriteLog("完成縮放光學解析度!", 1, richTextBoxGeneral);
+                                    });
+
+                                });
+                            }
+                            else
+                            {
+                                MessageBox.Show("請填入待處理影像資料夾路徑!", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("請填入輸出目的資料夾路徑!", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    }
+                case nameof(MoveCutImage):
+                    {
+                        if (!string.IsNullOrEmpty(Source_Folder.Text))
+                        {
+                            if (!string.IsNullOrEmpty(Output_Folder.Text))
+                            {
+                                string[] files = Directory.GetFiles(Source_Folder.Text);
+                                OpenCvSharp.Size size = new OpenCvSharp.Size(Convert.ToInt32(Crop_Width.Text), Convert.ToInt32(Crop_Length.Text));
+                                foreach (string filePath in files)
+                                {
+                                    Mat src = new Mat(filePath, ImreadModes.Color);
+                                    string file_name = System.IO.Path.GetFileNameWithoutExtension(filePath);
+                                    Do.MoveCutImage(src, Output_Folder.Text, file_name, size);
+                                }
+                                Logger.WriteLog("完成移動裁切影像!", 1, richTextBoxGeneral);
+                            }
+                            else
+                            {
+                                MessageBox.Show("請填入待處理影像資料夾路徑!", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("請填入輸出目的資料夾路徑!", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    }
+                case nameof(Stitching):
+                    {
+                        if (!string.IsNullOrEmpty(Source_Folder.Text))
+                        {
+                            if (!string.IsNullOrEmpty(Output_Folder.Text))
+                            {
+                                if (Do.CheckFileFormat(Source_Folder.Text))
+                                {
+                                    var color_type = (bool)Stitching_Color.IsChecked ? OpenCvSharp.ImreadModes.Color : OpenCvSharp.ImreadModes.Grayscale;
+                                    string direction = null;
+                                    if ((bool)Stitching_Top_to_Bottom.IsChecked)
+                                        direction = "Top to Bottom";
+                                    else if ((bool)Stitching_Bottom_to_Top.IsChecked)
+                                        direction = "Bottom to Top";
+                                    else if ((bool)Stitching_Left_to_Right.IsChecked)
+                                        direction = "Left to Right";
+                                    else if ((bool)Stitching_Right_to_Left.IsChecked)
+                                        direction = "Right to Left";
+                                    Do.Stitching(Source_Folder.Text, Output_Folder.Text, direction, color_type, Convert.ToDouble(Magnification.Text));
+                                    Logger.WriteLog("完成拼接影像!", 1, richTextBoxGeneral);
+                                }
+                                else
+                                {
+                                    MessageBox.Show("請將檔案名稱改為整數、副檔名改為bmp類型!", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("請填入待處理影像資料夾路徑!", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("請填入輸出目的資料夾路徑!", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+        #region Parameter Screen
+        private void Parameter_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Save_Config):
+                    {
+                        SaveConfig();
+                        Logger.WriteLog("完成儲存參數!", 1, richTextBoxGeneral);
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+
+    }
+}
