a83ede6a3a8d4fa88ba5e0a1ec8bf8f5e08c39ba
chimingkuei <chimingkuei@gmail.com>
2026-01-21 15:58:19 +0800

    EIMT Ver.1.0.0.0

---
+++ EIMT/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Reflection;
+
+namespace EIMT
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        System.Threading.Mutex mutex;
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool ret;
+            string projectName = Assembly.GetEntryAssembly()?.GetName().Name;
+            mutex = new System.Threading.Mutex(true, projectName, out ret);
+
+            if (!ret)
+            {
+                MessageBox.Show("已有一個程式執行!");
+                Environment.Exit(0);
+            }
+
+        }
+
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+
+        
+    }
+}
